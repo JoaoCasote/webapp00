a7d62c1051442487cb932faef42e775bc0d3913e
JoaoCasote <162324499+JoaoCasote@users.noreply.github.com>
2024-05-14 10:05:03 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -2,3 +2,13 @@ import streamlit as st
 
 st.title("A vida não é sobre quão duro você é capaz de bater, mas sobre quão duro você é capaz de apanhar e continuar indo em frente.")
 st.write("Rocky Balboa")
+from PIL import Image
+
+# Título da página
+st.title('Exemplo de Carregamento de Imagem')
+
+# Carregar a imagem
+image = Image.open('example_image.jpg')
+
+# Exibir a imagem
+st.image(image, caption='Imagem de exemplo', use_column_width=True)
