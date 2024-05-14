9459b6f9e66e37a2f2d2ec02e004178f32b111e0
JoaoCasote <162324499+JoaoCasote@users.noreply.github.com>
2024-05-14 09:52:31 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-st.title("Meu Primeiro Webapp Streamlit")
+st.title("A vida não é sobre quão duro você é capaz de bater, mas sobre quão duro você é capaz de apanhar e continuar indo em frente.")
 st.write("TESTE")
